be7d3de61d430fffde70026b3220f0ce0a899e58
Mikiyas Getachew <104720395+Micahget@users.noreply.github.com>
2022-09-12 17:24:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (var i = 0; i < names.length; i++) {
+ for (var i = 0; i < names.length; i++) {
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
@@ -58,13 +58,13 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
+
+    if (firstLetter === 'j') {
+      byeSpeaker(names[i]);
+    }
+    else {
+      helloSpeaker(names[i]);
+    }
   }
-}
-
-})
-();
-
+  
+  })();
